8f1f31da19b8bad4749d67e8de5bc6086094bdaa
skielynn <skie@skielynns-MacBook-Air.local>
2023-05-31 17:00:24 -0700

    dark mode vs light mode

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import './App.css';
 import './sass/styles.scss';
 import './css/styles.css';
 import Navbar from './components/navbar';
+import ThemeSwitch from './components/themeswitch';
+import { useState } from 'react';
 
 
 
 function App() {
+  const [theme, setTheme] = useState('light'); 
+
+  const handleThemeChange = (event) => {
+    setTheme(event.target.value);
+  };
   return (
-    <div>
+    <div className={`theme-${theme}`}>
+    <div className="theme-switch">
+      <select value={theme} onChange={handleThemeChange}>
+        <option value="light">Light Mode</option>
+        <option value="dark">Dark Mode</option>
+      </select>
+    </div>
       <Navbar />
       <div className="container">
         <h1 className="header">MY SASS PROJECT</h1>
